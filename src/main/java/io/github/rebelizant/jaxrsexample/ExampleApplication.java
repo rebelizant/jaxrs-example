8c2c0c3dd235f073c43eb8fa2fcd4d735f694577
Vadym Hula <vadym.hula@gmail.com>
2016-04-20 00:01:23 +0300

    add HelloWorld endpoint & config web.xml properly

--- src/main/java/io/github/rebelizant/jaxrsexample/ExampleApplication.java
+++ src/main/java/io/github/rebelizant/jaxrsexample/ExampleApplication.java
@@ -1,28 +1,18 @@
 package io.github.rebelizant.jaxrsexample;
 
-import io.github.rebelizant.jaxrsexample.rest.CustomerResourceImpl;
+import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author rebelizant
  *         Created on 18.04.16
  */
-@ApplicationPath("/example")
-public class ExampleApplication extends Application {
-
-    private Set<Object> singletons = new HashSet<>();
+@ApplicationPath("/")
+public class ExampleApplication extends ResourceConfig {
 
     public ExampleApplication() {
-        singletons.add(new CustomerResourceImpl());
-    }
-
-    @Override
-    public Set<Object> getSingletons() {
-        return singletons;
+        packages(true, "io.github.rebelizant.jaxrsexample.rest");
     }
 
 }
