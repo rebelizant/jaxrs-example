a41f95f6da87da2ce7c528b75a88069a9160a8b9
Vadym Hula <vadym.hula@gmail.com>
2016-04-27 17:23:33 +0000

    extends jax-rs Application class instead of ResourceConfig

--- src/main/java/io/github/rebelizant/jaxrsexample/ExampleApplication.java
+++ src/main/java/io/github/rebelizant/jaxrsexample/ExampleApplication.java
@@ -4,16 +4,26 @@ import io.github.rebelizant.jaxrsexample.rest.CustomerResourceImpl;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * @author rebelizant
  *         Created on 18.04.16
  */
 @ApplicationPath("/")
-public class ExampleApplication extends ResourceConfig {
+public class ExampleApplication extends Application {
+    
+    private Set<Object> singletons = new HashSet<>();
 
     public ExampleApplication() {
-        register(new CustomerResourceImpl());
+        singletons.add(new CustomerResourceImpl());
+    }
+    
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
     }
 
 }
